d870fbe26a48a0feacad43a9e7d995957c11e6fe
Vincent Liao <vincent.liao@protonmail.com>
2018-03-06 09:50:44 +0700

    small edits

--- embedding_utils.py
+++ embedding_utils.py
@@ -6,8 +6,7 @@ import zipfile
 dim_size = 100
 embedding_path = './data/glove.6B.zip'
 txt_path = './data/glove.6B/glove.6B.{}d.txt'.format(dim_size)
-# url = 'http://nlp.stanford.edu/data/glove.6B.zip'
-url = 'http://tabilab.cmpe.boun.edu.tr/datasets/sentiment_datasets/sanders-twitter-0.2.zip'
+url = 'http://nlp.stanford.edu/data/glove.6B.zip'
 
 def download_embedding():
 	os.system('wget {} -P {}'.format(url, './data/'))
