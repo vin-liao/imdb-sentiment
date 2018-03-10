e741447233dd3f31c2890fb5e57295456b64ddf2
Vincent Liao <vincent.liao@protonmail.com>
2018-03-10 15:19:20 +0700

    added multiple kernel size

--- cnn.py
+++ cnn.py
@@ -1,8 +1,8 @@
 import numpy as np
 import embedding_utils
 import data_utils
-from keras.models import Sequential
-from keras.layers import Dense, Embedding, GlobalMaxPooling1D, Dropout, Activation, Conv1D
+from keras.models import Sequential, Model
+from keras.layers import Dense, Embedding, GlobalMaxPooling1D, Dropout, Activation, Conv1D, Input, Concatenate, Flatten
 
 x_train, x_test, y_train, y_test = data_utils.load_data()
 word_index = data_utils.get_wi()
@@ -11,13 +11,23 @@ embedding_matrix = embedding_utils.load_embedding()
 dim_size = embedding_utils.get_dim()
 max_len = data_utils.get_max_len()
 
-model = Sequential()
-model.add(Embedding(vocab_size, dim_size, input_length=max_len, weights=[embedding_matrix], trainable=False))
-model.add(Conv1D(filters=3, kernel_size=4, activation='relu'))
-model.add(GlobalMaxPooling1D())
+input_shape = Input(shape=(max_len, ), dtype='int32')
+nb_filter = 2
+nb_kernel = [2, 3, 4]
+result = []
 
-model.add(Dense(1, activation='sigmoid'))
+embed = Embedding(vocab_size, dim_size, input_length=max_len, weights=[embedding_matrix], trainable=False)(input_shape)
+for i in nb_kernel:
+	conv = Conv1D(filters=nb_filter, kernel_size=i, activation='relu')(embed)
+	pool = GlobalMaxPooling1D()(conv)
+	result.append(pool)
 
+conc = Concatenate()(result)
+# print(conc.shape)
+# conc = Flatten()(conc)
+pred = Dense(1, activation='sigmoid')(conc)
+
+model = Model(inputs=input_shape, outputs=pred)
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 print(model.summary())
 
